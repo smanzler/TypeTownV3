da4476871fce73dfb80dcbf799a78e320d0f5456
simanzler <simanzler@gmail.com>
2024-10-14 08:20:47 -0400

    added monkeytype link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header-container">
-      <a>Inspired by monkeytype</a>
+      <a href="https://monkeytype.com/">Inspired by monkeytype</a>
     </div>
   );
 };
