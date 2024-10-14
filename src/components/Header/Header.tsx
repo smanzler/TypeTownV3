b6364281aa8d3c3d63dd7434fc3dba28136be370
simanzler <simanzler@gmail.com>
2024-10-14 08:25:14 -0400

    changed a tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header-container">
-      <a href="https://monkeytype.com/">Inspired by monkeytype</a>
+      <a href="https://monkeytype.com/" target="_blank">
+        Inspired by monkeytype
+      </a>
     </div>
   );
 };
